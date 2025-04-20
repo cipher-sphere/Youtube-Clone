ea136805daf8880ae587bf5bafb876aafd1efde0
cipher-sphere <shivam0hapur@gmail.com>
2025-04-20 14:46:13 +0530

    error debug

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Search from "./components/Search";
-import PlayingVideo from "./components/PlayingVideo";
+import PlayingVideo from "./components/Play.jsx";
 import { useAuth } from "./context/AuthProvider";
 import Loading from "./loader/Loading";
 
